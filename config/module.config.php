4d5e1822a145290c23d1574ea8229a44ebdbca5f
Johan <joacub@gmail.com>
2014-03-01 14:52:03 +0100

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- config/module.config.php
+++ config/module.config.php
@@ -23,6 +23,9 @@ return array(
         ),
     ),
     'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
         'template_map' => include __DIR__  .'/../template_map.php',
     ),
     'controllers' => array(
